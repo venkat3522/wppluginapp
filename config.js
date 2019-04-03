bf86b642e44209a2011039c358c879aa82bc92c0
venkat3522 <v.venki05@gmail.com>
2019-04-03 11:03:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'dailypandas',
+		wp_ws_url : 'https://dataviolet.com/shop/wp-appkit-api/dailypandas',
+		wp_url : 'https://dataviolet.com/shop',
+		theme : 'q-android',
+		version : '1.0',
+		app_type : 'phonegap-build',
+		app_title : 'DailyPandas',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : '8ZjG})2E#mZdEJOvb0c;#5eMa|V}3AZPY2b6[3*aD:;RA!Q.>#QShu[C;R^d-du$',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
